a443e7cf25e941ff4f002f1edffeb8cce868740e
hobo0xcc <hobo0xcc@gmail.com>
2020-04-11 13:29:36 +0900

    add some comment

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use ansi_term::Color::Red;
 use dirs::*;
 
 struct Env {
-    exit_code: i32,
-    cur_dir: PathBuf,
+    exit_code: i32, // Exit code of previous command.
+    cur_dir: PathBuf, // Current Directory.
 }
 
 impl Env {
@@ -31,11 +31,18 @@ impl Env {
 
 fn builtin_cd(env: &mut Env, dir: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut path = PathBuf::new();
+
+    // When directory name isn't already Absolute path or Relative path,
+    // make directory name as a Relative path.
     if !dir.starts_with("/") && !dir.starts_with("./") && !dir.starts_with("..") {
         path.push("./");
     }
     path.push(dir);
+
+    // Convert path to Absolute path.
+    // https://doc.rust-lang.org/std/fs/fn.canonicalize.html
     let absolute_path = fs::canonicalize(path)?;
+
     std::env::set_current_dir(absolute_path)?;
     env.cur_dir = std::env::current_dir().unwrap();
     Ok(())
@@ -60,14 +67,7 @@ fn exec(env: &mut Env, commands: Vec<String>) -> Result<(), Box<dyn std::error::
         "cd" => builtin_cd(env, commands.get(1).unwrap())?,
         _ => exec_command(env, program_name, &commands[1..])?,
     }
-    // let mut cmd = Command::new(program_name)
-    //     .args(&commands[1..])
-    //     .stdin(Stdio::inherit())
-    //     .stdout(Stdio::inherit())
-    //     .current_dir(&mut env.cur_dir)
-    //     .spawn()?;
-    // let status = cmd.wait().unwrap();
-    // env.exit_with(status.code().unwrap());
+
     Ok(())
 }
 
@@ -78,20 +78,24 @@ fn main() {
 
     loop {
         let mut buf = String::new();
+
         if cur_env.exit_code != 0 {
             print!("{} ", Red.paint(format!("{}", cur_env.exit_code)));
         }
         std::env::set_current_dir(&cur_env.cur_dir).expect("Error occurred");
         print!("{}> ", cur_env.cur_dir.display());
         io::stdout().flush().expect("Error occurred");
+
         reader.read_line(&mut buf).expect("Error occurred");
         if buf.starts_with("exit") {
             process::exit(0);
         }
-        if buf.len() == 0 {
+
+        let commands = buf.split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>();
+        if commands.len() == 0 {
             continue;
         }
-        let commands = buf.split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>();
+
         match exec(&mut cur_env, commands) {
             Ok(_) => {},
             Err(msg) => println!("{}", msg),
